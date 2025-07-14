a3c2ef6b72bebc915ce7cb11179ef7e9b88bfbef
Linh Vo Tran Nha <vtrnnhlinh@gmail.com>
2025-07-14 21:43:58 +0700

    mini model 1

--- datasets/csv_to_parquet.py
+++ datasets/csv_to_parquet.py
@@ -1,14 +1,6 @@
 import pandas as pd
 
-# fruits.parquet
-df1_fruits = pd.read_parquet("../datasets/dataset_1/fruits.parquet")
-df1_fruits = df1_fruits.rename(columns={"fruit": "ingredients"})
-df1_fruits.to_parquet("../datasets/dataset_1/fruits.parquet")
-# vegetables_seafood.parquet
-df1_vegetables_seafood = pd.read_parquet("../datasets/dataset_1/vegetables_seafood.parquet")
-df1_vegetables_seafood = df1_vegetables_seafood.rename(columns={"Food and Serving": "ingredients"})
-df1_vegetables_seafood.to_parquet("../datasets/dataset_1/vegetables_seafood.parquet")
-# vegetables.parquet
-df1_vegetables = pd.read_parquet("../datasets/dataset_1/vegetables.parquet")
-df1_vegetables = df1_vegetables.rename(columns={"Name": "ingredients"})
-df1_vegetables.to_parquet("../datasets/dataset_1/vegetables.parquet")
+# Load CSV
+df = pd.read_csv("datasets/dataset_1/vegetables.csv", encoding="latin1")
+# Save as Parquet
+df.to_parquet("datasets/dataset_1/vegetables.parquet", engine="pyarrow", index=False)
