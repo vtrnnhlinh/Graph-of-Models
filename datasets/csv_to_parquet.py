66c6ee3b55c3d91379188e8ee0de1a1a7e6a8b72
Linh Vo Tran Nha <vtrnnhlinh@gmail.com>
2025-07-16 01:07:37 +0700

    clean

--- datasets/csv_to_parquet.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-# Load CSV
-df = pd.read_csv("datasets/dataset_1/vegetables.csv", encoding="latin1")
-# Save as Parquet
-df.to_parquet("datasets/dataset_1/vegetables.parquet", engine="pyarrow", index=False)
